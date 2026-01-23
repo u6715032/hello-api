c78ac1239d372cdba8cc73bd9fd5d569bf8363ba
u6715032 <u6715032@au.edu>
2026-01-23 15:11:03 +0700

    Enable CORS for Hello API

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,7 +1,17 @@
 import { NextResponse } from "next/server";
+import corsHeaders from "@/lib/cors";
 
-export async function GET() {
-  return NextResponse.json({
-    message: "hello world",
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 200,
+    headers: corsHeaders,
   });
 }
+
+export async function GET() {
+  return NextResponse.json(
+    { message: "hello world" },
+    { headers: corsHeaders }
+  );
+}
+
